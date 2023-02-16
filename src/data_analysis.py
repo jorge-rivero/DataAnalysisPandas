4b436b4010c41b8234f1004e0130fba2ac6b4c4f
jorge.rivero <jlrivero85@gmail.com>
2023-02-16 08:19:22 -0500

    requirements.txt: Adding Jupyter Lab dependencies Pandas: ipynb: Creating Jupyter notebook

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,2 +1,9 @@
 import pandas as pd
-print(pd.__version__)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
+df = pd.read_csv('../data/survey_results_public.csv')
+schema_df = pd.read_csv('../data/survey_results_schema.csv')
+
+
